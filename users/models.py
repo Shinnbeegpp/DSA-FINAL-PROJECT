10849f9432b25e5d66179fd218498285940e3a93
Shinnbeegpp <marfajonard17@gmail.com>
2025-11-24 17:34:35 +0800

    DONE-LOGINSYSTEM
    DONE LOGINSYSTEM

--- users/models.py
+++ users/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
+    ACCT_TYPES = [
+        ('commissioner', 'Commissioner'),
+        ('commissionee', 'Commissionee'),
+    ]
+    account_type = models.CharField(max_length=21, choices=ACCT_TYPES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.account_type}"
