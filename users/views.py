ba405ef0f0828976b855b4efe402267ca5d80939
Ryuuji <butterscarce@gmail.com>
2025-11-27 08:16:10 +0800

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import UserProfile
