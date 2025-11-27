8c6d65acb44b3625ef2ece6bbc034bac10911c6b
Shinnbeegpp <marfajonard17@gmail.com>
2025-11-27 22:57:45 +0800

    DONE ALL PROFILE SETTINGS
    DONREODNOENOEND

--- users/views.py
+++ users/views.py
@@ -211,3 +211,6 @@ def applied_jobs(request):
 #@login_required 
 def favorite_jobs(request):
     return render(request,'favorite_jobs.html')
+
+def saved_candidates(request):
+    return render(request,'saved_candidates.html')
